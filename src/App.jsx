2945387800a91bebe4fe5bfaead497815a48a8fe
Hritik Chauhan <hritikchji@gmail.com>
2025-07-26 18:52:33 +0530

    change theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/toolbar/lib/styles/index.css';
 
 function App() {
-  const [themeMode, setThemeMode] = useState('dark');
+  const [themeMode, setThemeMode] = useState('light');
   const location = useLocation();
 
   const darkTheme = () => setThemeMode('dark');
